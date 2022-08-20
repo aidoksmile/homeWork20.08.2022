690be8736d6f13df83a227928c32df891ec162fd
Aida <Aida@MacBook-Air.local>
2022-08-20 16:08:12 +0600

    Найти максимальное из трех чисел

---
+++ 3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Найти максимальное из трех чисел
+
+Console.Write("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int number3 = int.Parse(Console.ReadLine());
+int max = 0;
+if (number1 > number2) 
+{
+    max = number1;
+}
+else
+{
+    max = number2;
+}
+if (number3 > max) 
+{
+    max = number3;
+}
+Console.Write("Максимальное число = ");
+Console.WriteLine(max);
