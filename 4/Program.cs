64c154cb5c476e751ea7b014297c58ec7d855031
Aida <Aida@MacBook-Air.local>
2022-08-20 16:08:32 +0600

    Выяснить является ли число чётным

---
+++ 4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+if (n % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
