1ddb94bb56124196fcddfb8348b2e0e98ba0aeac
Aida <Aida@MacBook-Air.local>
2022-08-20 16:09:37 +0600

    Удалить вторую цифру трёхзначного числа

---
+++ 8/Program.cs
@@ -0,0 +1,8 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное число n: ");
+int n = int.Parse(Console.ReadLine());
+int firstNumber = n / 100;
+int lastNumber = n % 10;
+Console.Write("При удалении второй цифры получаем число: ");
+Console.WriteLine(firstNumber * 10 + lastNumber); 
