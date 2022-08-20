3e71729a4b3ede16b4486096b8cb15db685329af
Aida <Aida@MacBook-Air.local>
2022-08-20 16:10:08 +0600

    Найти третью цифру числа или сообщить, что её нет

---
+++ 10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+int n = new Random().Next(1,999);
+Console.Write("Заданное число: ");
+Console.WriteLine(n);
+if (n < 100)
+{
+    Console.WriteLine("В числе нет третьей цифры");
+}
+else
+{
+    Console.Write("Третья цифра в числе: ");
+    Console.WriteLine(n % 10);
+}
