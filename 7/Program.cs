16b8b79c6982ba4479b1499b5ffcc06d5e853efd
Aida <Aida@MacBook-Air.local>
2022-08-20 16:09:20 +0600

    Дано число из отрезка [10, 99]. Показать наибольшую цифру числа

---
+++ 7/Program.cs
@@ -0,0 +1,18 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int n = new Random().Next(10,99);
+Console.Write("Данное число: ");
+Console.WriteLine(n);
+int a = n / 10;
+int b = n % 10;
+int max = 0;
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+Console.Write("Наибольшая цифра числа: ");
+Console.WriteLine(max);
