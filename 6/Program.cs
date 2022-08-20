0d56237a9ea48cb8495a35c79b9adf2c7a825ffa
Aida <Aida@MacBook-Air.local>
2022-08-20 16:09:03 +0600

    Показать вторую цифру трёхзначного числа

---
+++ 6/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное число n: ");
+int n = int.Parse(Console.ReadLine());
+int secondNumber = (n / 10) % 10;
+Console.Write("Второе число: ");
+Console.WriteLine(secondNumber); 
